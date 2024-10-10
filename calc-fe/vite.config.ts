7b31880cd6bae8ff170226a5e44e0172647a53cc
JigneshBotla <159447423+JigneshBotla@users.noreply.github.com>
2024-10-10 12:43:36 +0530

    change

--- calc-fe/vite.config.ts
+++ calc-fe/vite.config.ts
@@ -4,8 +4,7 @@ import eslint from 'vite-plugin-eslint';
 import { defineConfig } from "vite"
  
 export default defineConfig({
-  root: '.',  // Add this to ensure it's pointing to the right directory
-  plugins: [react(), eslint()],
+  plugins: [react(),eslint()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
